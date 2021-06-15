a82cda6d61801c7d2ec5672ac4835a42795a6990
Sebastian Karlsson <ianertson@gmail.com>
2021-06-15 13:24:37 +0200

    cache

--- lyrics/__init__.py
+++ lyrics/__init__.py
@@ -1,12 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import os
 
+
+cache = json.loads(open("cache.json").read())\
+    if os.path.isfile("cache.json") else {}
 
 SEARCH_URL = "https://search.azlyrics.com/search.php?q={artist}+{title}"
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
 }
+
+
+def gethash(artist, title):
+    return artist.lower() + title.lower()
 
 
 def get_lyrics_from_link(link):
@@ -22,12 +31,23 @@ def get_lyrics_from_link(link):
     # body > div.container.main-page > div > div.col-xs-12.col-lg-8.text-center > div:nth-child(8)
 
 def search(artist, title):
+    h = gethash(artist, title)
+    
+    if h in cache:
+        return cache[h]
+
     r = requests.get(SEARCH_URL.format(artist=artist, title=title), headers=HEADERS)
     doc = BeautifulSoup(r.text, 'html.parser')
     table = doc.select_one(".table.table-condensed")
+
+    if not table:
+        return []
     links = table.select("tr a")
 
-    return list(map(get_lyrics_from_link, list(filter(lambda x: "http" in x, [a.get("href") for a in links]))))
+    if not links:
+        return []
 
+    data = list(map(get_lyrics_from_link, list(filter(lambda x: "http" in x, [a.get("href") for a in links]))))
+    cache[h] = data
 
-
+    return data
